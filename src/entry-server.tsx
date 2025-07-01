18dd4a41565ade434a497ceba0cdc12d12694231
Dhruv Dhaduk <dhruv.dhaduk@simformsolutions.com>
2025-07-01 14:35:18 +0530

    Setup the prettier

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -1,12 +1,12 @@
-import { StrictMode } from 'react'
-import { renderToString } from 'react-dom/server'
-import App from './App'
+import { StrictMode } from 'react';
+import { renderToString } from 'react-dom/server';
+import App from './App';
 
 export function render(_url: string) {
-  const html = renderToString(
-    <StrictMode>
-      <App />
-    </StrictMode>,
-  )
-  return { html }
+    const html = renderToString(
+        <StrictMode>
+            <App />
+        </StrictMode>
+    );
+    return { html };
 }
